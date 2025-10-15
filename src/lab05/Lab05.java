1007a5338f42527fbf0b8de423bbeaea35d9d503
Quentin Proulx <6324569@edu.vaniercollege.qc.ca>
2025-10-15 12:11:52 -0400

    Last Commit Class

--- src/lab05/Lab05.java
+++ src/lab05/Lab05.java
@@ -4,17 +4,46 @@
  */
 package lab05;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
 /**
  *
  * @author 6324569
  */
-public class Lab05 {
+public class Lab05 extends Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Select Bag Style: ");
+
+        ListView choices = new ListView();
+        choices.setPrefWidth(100);
+        choices.setPrefHeight(200);
+        
+        GridPane gridPane = new GridPane();
+        gridPane.add(label, 0, 0);
+        gridPane.add(choices, 1, 0);
+        
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        
+        BorderPane root = new BorderPane(gridPane);
+        Scene scene = new Scene(root, 500, 500);
+        
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
     
 }

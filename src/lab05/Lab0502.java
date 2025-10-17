764f22c0aee94767ad3b872b09a73d95c425ca04
QuentinProulx <quentin.proulx@gmail.com>
2025-10-17 03:07:42 -0400

    Started task 2

---
+++ src/lab05/Lab0502.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab05;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author quent
+ */
+public class Lab0502 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) {
+        
+        stage.setTitle("Restaurant Application");
+        stage.setResizable(false);
+        stage.show();
+    }
+    
+}
